e7ea7a54f1969499e531650abfe0e2152780c472
hossain <hossainahamedk@gmail.com>
2024-12-09 21:36:04 +0600

    added google fonts to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@
 export default function Home() {
   return (
     <>
-      Home
+      <div className="font-figtree">Home</div>
+      <div className="font-roboto">Home</div>
+      <div className="font-poppins">Home</div>
+      <div className="font-dm_Sans">Home</div>
+      <div className="font-inter">Home</div>
+      <div className="font-urbanist">Home</div>
     </>
   );
 }
