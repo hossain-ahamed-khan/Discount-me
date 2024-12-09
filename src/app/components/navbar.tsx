9e290a8d6bbd87277cf659af31e5fad29f0189e1
hossain <hossainahamedk@gmail.com>
2024-12-10 03:21:34 +0600

    added search bar to navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../../public/images/main-logo.png";
+import search from "../../../public/images/Search.png";
 
 export const Navbar = () => {
     return (
@@ -9,15 +10,28 @@ export const Navbar = () => {
                 <div>
                     <Image
                         src={logo}
-                        width={200}
+                        width={250}
                         height={100}
                         alt="main logo image"
                     />
                 </div>
                 <div>
-                    search
+                    <div className="relative flex border rounded-3xl">
+                        <Image
+                            className="absolute top-3.5 left-2.5"
+                            src={search}
+                            width={20}
+                            height={20}
+                            alt="main logo image"
+                        />
+                        <input className="w-96 pl-9 rounded-3xl focus:outline-none" type="text" placeholder="Search" />
+                        <button className="bg-[#FF7D29] py-3 px-6 text-white rounded-r-3xl">Search</button>
+                    </div>
                 </div>
                 <nav>
+                    <div>
+                        <h1>dropdown menu</h1>
+                    </div>
                     <Link href="/" className="text-[#1D242D] ">
                         Coupon
                     </Link>
