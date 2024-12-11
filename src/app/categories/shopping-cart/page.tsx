74c59d359e2fd00736637b0da5039cc0fc93a85d
hossain <hossainahamedk@gmail.com>
2024-12-11 17:55:59 +0600

    added my shopping cart

--- src/app/categories/shopping-cart/page.tsx
+++ src/app/categories/shopping-cart/page.tsx
@@ -1,7 +1,126 @@
+import Image from "next/image";
+import shoppingCartBanner from "../../../../public/images/shopping-cart-banner.png";
+import frizeImg from "../../../../public/images/frize-main.png";
+import tvImg from "../../../../public/images/TV main.png";
+import IncrementDecrementBtn from "@/app/components/incrementDecrementBtn";
+
 export default function ShoppingCart() {
     return (
         <div>
-            shopping cart
+            <div>
+                <Image
+                    src={shoppingCartBanner}
+                    sizes="100vw"
+                    style={{
+                        width: '100%',
+                        height: 'auto',
+                    }}
+                    alt="main logo image"
+                />
+            </div>
+
+            <div className="w-full lg:w-4/5 mx-auto">
+                <h1 className="text-3xl text-[#1A1A1A] font-bold my-5">My Shopping Cart</h1>
+                <div className="flex-none lg:flex gap-3 my-3 lg:my-6">
+                    <div className="w-full lg:w-3/4 bg-white rounded-md border text-[#1A1A1A]">
+                        <div className="overflow-x-auto">
+                            <table className="table">
+                                {/* head */}
+                                <thead>
+                                    <tr>
+                                        <th>PRODUCT</th>
+                                        <th>PRICE</th>
+                                        <th>QUANTITY</th>
+                                        <th>SUBTOTAL</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {/* row 1 */}
+                                    <tr>
+                                        <td>
+                                            <div className="flex items-center gap-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squire h-16 w-16">
+                                                        <Image
+                                                            src={frizeImg}
+                                                            width={100}
+                                                            height={100}
+                                                            alt="product image"
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold">Hart Hagerty</div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $14.00
+                                        </td>
+                                        <td className="h-10 w-20">
+                                            <IncrementDecrementBtn />
+                                        </td>
+                                        <td>
+                                            $14.00
+                                        </td>
+                                    </tr>
+                                    {/* row 2 */}
+                                    <tr>
+                                        <td>
+                                            <div className="flex items-center gap-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squire h-16 w-16">
+                                                        <Image
+                                                            src={frizeImg}
+                                                            width={100}
+                                                            height={100}
+                                                            alt="product image"
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold">Brice Swyre</div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            $14.00
+                                        </td>
+                                        <td className="h-10 w-20">
+                                            <IncrementDecrementBtn />
+                                        </td>
+                                        <td>
+                                            $14.00
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <div className="w-full flex justify-between my-3 px-3 text-[#4D4D4D]">
+                                <button className="bg-[#F2F2F2] rounded-full py-2 px-4">Return to shop</button>
+                                <button className="bg-[#F2F2F2] rounded-full py-2 px-4">Update Cart</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="w-full lg:w-1/4 bg-white rounded-md p-4 text-[#1A1A1A] font-poppins border">
+                        <h1 className="text-xl font-medium">Cart Total</h1>
+                        <div className="flex justify-between p-3 border-b-2">
+                            <p>Subtotal:</p>
+                            <p className="font-semibold">$84.00</p>
+                        </div>
+                        <div className="flex justify-between p-3 border-b-2">
+                            <p>Shipping:</p>
+                            <p className="font-semibold">Free</p>
+                        </div>
+                        <div className="flex justify-between p-3">
+                            <p>Total:</p>
+                            <p className="font-semibold">$84.00</p>
+                        </div>
+                        <button className="bg-[#EA5326] w-full mt-3 p-2 rounded-full text-white">Proceed to checkout</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/app/restaurants/[restaurantId]/items/[itemId]/page.tsx
+++ src/app/restaurants/[restaurantId]/items/[itemId]/page.tsx
@@ -4,81 +4,95 @@ import itemImg from "../../../../../../public/images/frize-main.png";
 import IncrementDecrementBtn from "@/app/components/incrementDecrementBtn";
 import shoppingBag from "../../../../../../public/images/shopping-bag.png";
 import shoppingCart from "../../../../../../public/images/shopping_cart_icon 1.png";
+import itemDetailsBanner from "../../../../../../public/images/item-details-banner.png";
 
 
 export default function ItemDetails() {
 
     return (
-        <div className="w-full lg:w-4/5 mx-auto">
-            <div className="flex-none lg:flex my-3 lg:my-10">
-                <div className="flex-1">
-                    <Image
-                        src={itemImg}
-                        width={500}
-                        height={200}
-                        alt="main logo image"
-                    />
-                </div>
-                <div className="flex-1">
-                    <div className="flex gap-2">
-                        <h1 className="font-figtree text-[#1A1A1A] text-3xl font-semibold">Smart Freezer</h1>
-                        <p className="p-2 text-[#FF7D29] bg-[#FDEEE9] rounded-3xl">In Stock</p>
-                    </div>
-                    <div className="flex gap-3 mt-6">
-                        <div className="rating">
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input
-                                type="radio"
-                                name="rating-2"
-                                className="mask mask-star-2 bg-orange-400"
-                                defaultChecked />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        </div>
-                        <p className="text-[#666666] ">4 Review</p>
-                    </div>
-                    <div className="py-6 border-b-2">
-                        <p className="text-[#FF7D29] text-2xl font-medium">$17.28</p>
-                    </div>
-                    <div className="py-6">
-                        <p className="text-[#1A1A1A] font-medium">Category: <span className="text-[#808080]">Freeze</span></p>
-                    </div>
-                    <p className="pb-6">
-                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar.
-                    </p>
-                    <div className="flex gap-3">
-                        <div className="flex bg-white rounded-full">
-                            <IncrementDecrementBtn />
-                        </div>
-                        <div className="w-96 bg-[#FF7D29] rounded-full flex gap-2 justify-center items-center">
-                            <button className="text-white text-poppins font-semibold">Checkout</button>
-                            <Image
-                                src={shoppingBag}
-                                width={20}
-                                height={20}
-                                alt="main logo image"
-                            />
-                        </div>
-                        <div className="px-4 bg-[#DFE1E3] rounded-full flex justify-center items-center">
-                            <Image
-                                src={shoppingCart}
-                                width={25}
-                                height={25}
-                                alt="main logo image"
-                            />
-                        </div>
-                    </div>
-                </div>
+        <div>
+            <div>
+                <Image
+                    src={itemDetailsBanner}
+                    sizes="100vw"
+                    style={{
+                        width: '100%',
+                        height: 'auto',
+                    }}
+                    alt="main logo image"
+                />
             </div>
+            <div className="w-full lg:w-4/5 mx-auto">
+                <div className="flex-none lg:flex my-3 lg:my-10">
+                    <div className="flex-1">
+                        <Image
+                            src={itemImg}
+                            width={500}
+                            height={200}
+                            alt="main logo image"
+                        />
+                    </div>
+                    <div className="flex-1">
+                        <div className="flex gap-2">
+                            <h1 className="font-figtree text-[#1A1A1A] text-3xl font-semibold">Smart Freezer</h1>
+                            <p className="p-2 text-[#FF7D29] bg-[#FDEEE9] rounded-3xl">In Stock</p>
+                        </div>
+                        <div className="flex gap-3 mt-6">
+                            <div className="rating">
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input
+                                    type="radio"
+                                    name="rating-2"
+                                    className="mask mask-star-2 bg-orange-400"
+                                    defaultChecked />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                                <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                            </div>
+                            <p className="text-[#666666] ">4 Review</p>
+                        </div>
+                        <div className="py-6 border-b-2">
+                            <p className="text-[#FF7D29] text-2xl font-medium">$17.28</p>
+                        </div>
+                        <div className="py-6">
+                            <p className="text-[#1A1A1A] font-medium">Category: <span className="text-[#808080]">Freeze</span></p>
+                        </div>
+                        <p className="pb-6">
+                            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam, blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar.
+                        </p>
+                        <div className="flex gap-3">
+                            <div className="flex bg-white rounded-full">
+                                <IncrementDecrementBtn />
+                            </div>
+                            <div className="w-96 bg-[#FF7D29] rounded-full flex gap-2 justify-center items-center">
+                                <button className="text-white text-poppins font-semibold">Checkout</button>
+                                <Image
+                                    src={shoppingBag}
+                                    width={20}
+                                    height={20}
+                                    alt="main logo image"
+                                />
+                            </div>
+                            <div className="px-4 bg-[#DFE1E3] rounded-full flex justify-center items-center">
+                                <Image
+                                    src={shoppingCart}
+                                    width={25}
+                                    height={25}
+                                    alt="main logo image"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-            <div className="my-3 lg:my-20">
-                <h1 className="text-[#232323] text-3xl font-bold font-figtree mb-3">Customer Feedback</h1>
-                <div className="w-full lg:w-3/4">
-                    <ReviewCard />
-                    <ReviewCard />
-                    <ReviewCard />
-                    <ReviewCard />
+                <div className="my-3 lg:my-20">
+                    <h1 className="text-[#232323] text-3xl font-bold font-figtree mb-3">Customer Feedback</h1>
+                    <div className="w-full lg:w-3/4">
+                        <ReviewCard />
+                        <ReviewCard />
+                        <ReviewCard />
+                        <ReviewCard />
+                    </div>
                 </div>
             </div>
         </div>
